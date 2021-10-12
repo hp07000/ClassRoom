2b8763db77f66de447d94d852388040013fe00c6
Puchchakayala,Harshini Reddy <thutiswetha@gmail.com>
2021-10-11 19:07:11 -0500

    New class

---
+++ Class/javaScript/school.js
@@ -0,0 +1,37 @@
+class SelectionCommittee {
+    staffs
+    applications
+    accecptApplication(application) {
+    
+    }
+}
+class Subject {
+    credit = 0
+    name = ""
+    prerequesties = undefined
+    type = ""
+    description = ""
+}
+
+class Student {
+    name = ""
+    id = ""
+    completedSubjects = []
+}
+
+class Staff {
+    name = ""
+    id = ""
+    validateApplication (application) {
+
+    }
+}
+
+class Application {
+    student
+    intestedSubjects = []
+}
+//Normal courses
+   // Band, Choir, Art
+//High-school credits
+   // Any launguage, Av-tech, Robotics;
